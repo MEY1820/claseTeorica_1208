cf03dbb6a18336434aaaeae4b84c5f13aca99576
Meylin Reyes <reyesmeylin20@gmail.com>
2021-08-12 21:27:09 -0600

    Ejercicio 1

--- claseTeorica_1208/Program.cs
+++ claseTeorica_1208/Program.cs
@@ -10,6 +10,53 @@ namespace claseTeorica_1208
     {
         static void Main(string[] args)
         {
+            //llamado al metodo loops
+            loops();
+
+            Console.ReadLine();
         }
+
+        //bucles
+        public static void loops()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            //ciclo while
+            int i = 0;
+            Console.WriteLine("IMPRIME ELEMENTOS CON WHILE");
+            while (i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+                i = i + 1; //i++
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON DO-WHILE");
+            int inc = 0;
+            do
+            {
+                Console.WriteLine(numbers[inc]);
+                inc++;
+            } while (inc < numbers.Length);
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON FOR");
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine("Posicion {0} : {1}", j, numbers[j]);
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON FOREACH");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
     }
 }
+
+   
---
+++ claseTeorica_1208/Semana 3 ejercicio 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTeorica_1208
+{
+    class Semana_3_ejercicio_1
+    {
+        static void Main(string[] args)
+        {
+            // 1- escribir un ciclo que permita capturar los valores
+            // a almacenar en el arreglo de numbers con un tamaño de 10
+
+            int[] numbers = new int[10];
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine($"introduce el numero");
+                numbers[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Los numeros");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
